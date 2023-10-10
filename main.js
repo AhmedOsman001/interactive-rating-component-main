5cae8f8ae4bf66ebdc9015e9987523f0afec313a
AhmedOsman001 <145006878+AhmedOsman001@users.noreply.github.com>
2023-10-10 21:02:07 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,24 @@
-
 let lol =0;
 
 const container = document.getElementById("container");
 
-// Add a click event listener to the container
 container.addEventListener("click", function(event) {
-    // Check if the clicked element has the "clickable" class
+    // Check if the clicked element has the "stars" class
     if (event.target.classList.contains("stars")) {
         // Get the text content of the clicked element
         lol = event.target.textContent; 
         $("#rate").text(lol)
         
     }
-
-    console.log(lol)
+    
+    //Check if there is a clicked element
     if (lol !== 0) {
         $(document).ready(function() {
         
             $("#submit").click(function(){
-    
             $(".before").css({"display":"none"});
             $(".after").addClass("content");
-            
-            
+       
       });
     })
     
